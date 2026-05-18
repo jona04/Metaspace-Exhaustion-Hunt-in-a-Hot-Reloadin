67db8be5067f051d65989d0579dac0811fa2c68e
root <root@jonatas-a-java-daemon-that-hot-reloads-text-p-15552f-0.jonatas-a-java-daemon-that-hot-reloads-text-p-15552f.workspaces.svc.cluster.local>
2026-05-18 01:13:36 +0000

    s

---
+++ out/harbor/run-66e72c8e4ec4/2026-05-18__00-56-47/task__NGHD3hs/agent/shell_snapshots/019e3896-5aac-7012-9f5d-7cc187409314.1779065838253041860.sh
@@ -0,0 +1,29 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+# Functions
+
+# setopts 3
+set -o braceexpand
+set -o hashall
+set -o interactive-comments
+
+# aliases 0
+
+# exports 17
+declare -x ANTHROPIC_API_KEY="sk-iMXL8pailRFmZSXVgZmQCQ"
+declare -x ANTHROPIC_BASE_URL="https://litellm.vetto-labs.dev"
+declare -x CODEX_HOME="/logs/agent"
+declare -x CODEX_MANAGED_BY_NPM="1"
+declare -x HOME="/root"
+declare -x HOSTNAME="296a51bd6116"
+declare -x LITELLM_PROXY_API_BASE="https://litellm.vetto-labs.dev"
+declare -x LITELLM_PROXY_API_KEY="sk-iMXL8pailRFmZSXVgZmQCQ"
+declare -x NVM_BIN="/root/.nvm/versions/node/v22.22.3/bin"
+declare -x NVM_CD_FLAGS=""
+declare -x NVM_DIR="/root/.nvm"
+declare -x NVM_INC="/root/.nvm/versions/node/v22.22.3/include/node"
+declare -x OPENAI_API_BASE="https://litellm.vetto-labs.dev/v1"
+declare -x OPENAI_API_KEY="sk-iMXL8pailRFmZSXVgZmQCQ"
+declare -x OPENAI_BASE_URL="https://litellm.vetto-labs.dev/v1"
+declare -x PATH="/logs/agent/tmp/arg0/codex-arg0wZrrde:/root/.nvm/versions/node/v22.22.3/lib/node_modules/@openai/codex/node_modules/@openai/codex-linux-x64/vendor/x86_64-unknown-linux-musl/path:/root/.nvm/versions/node/v22.22.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+declare -x SHLVL="2"
